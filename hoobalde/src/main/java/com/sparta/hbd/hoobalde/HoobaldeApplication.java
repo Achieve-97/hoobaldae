02dbc4e12b2d6c27285acd54340260d5bedb010b
Achieve <113897434+Achieve-97@users.noreply.github.com>
2023-01-16 19:05:22 +0900

    Add files via upload

---
+++ hoobalde/src/main/java/com/sparta/hbd/hoobalde/HoobaldeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hbd.hoobalde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoobaldeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoobaldeApplication.class, args);
+    }
+
+}
---
+++ hoobalde/src/main/java/com/sparta/hbd/hoobalde/prac01/Prac01.java
@@ -0,0 +1,34 @@
+package com.sparta.hbd.hoobalde.prac01;
+
+public class Prac01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+
+        int num = 3;
+        String str = "반갑습니다";
+
+        System.out.println(str);
+        System.out.println(num);
+
+        str = "오늘은 화요일";
+        System.out.println(str);
+
+        char c = 'A';
+
+        int a = 10;
+        double b = 1.234;
+        float d = 1.234F;
+
+//        자바의 기본 자료형
+//		boolean : 1바이트, 논리값(true => 참, false => 거짓)을 기억한다.
+//		byte    : 1바이트, 파일 전송 => 파일 다운로드
+//		char    : 2바이트, 문자 1개 => 자바는 UniCode를 사용한다. //null값없음
+//		short   : 2바이트, -32768 ~ 32767 사이의 정수를 저장할 수 있다.
+//		int     : 4바이트, -2147483648 ~ 2147483647 사이의 정수를 저장할 수 있다.
+//		long    : 8바이트, -2의 63승 ~ 2의 63승 -1 사이의 정수를 저장할 수 있다.
+//		float   : 4바이트, 단정도 실수, 소수점 아래로 6자리 정도를 표현한다.
+//		double  : 8바이트, 배정도 실수, 소수점 아래로 16자리 정도를 표현한다.
+
+    }
+}
